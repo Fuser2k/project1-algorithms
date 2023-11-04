aa0819cee176e41c8fffeeeb26ef334509419264
Olcay Duzgun <88849954+Fuser2k@users.noreply.github.com>
2023-11-04 03:28:07 +0100

    working on heap sorting

--- code/src/sorting/impl_sort/heap.cpp
+++ code/src/sorting/impl_sort/heap.cpp
@@ -1,7 +1,59 @@
 #include "../sorting.hpp"
 #include <algorithm>
 
+/*
+Complexity: O(nlog n)
+
+Description:
+[
+    We are generating a binary tree using our numbers.
+    Our first number will be root at the start and next element added as left element and next one is right element.
+    Then we add other numbers like this (adding other numbers to left as left and right again)
+    And we check if greater child element is greater than parent, if it is we swap them.
+    We continue doing this for generating max heap.
+
+    After generating final max heap,
+    we remove root (largest number) from heap and swap it with last element in vector.
+
+]
+*/
+
+// Forward declerations
+void gen_heap(std::vector<int> &heap, int root);
+
 std::vector<int> CSorting::Heap()
 {
-    throw("TODO: not implemented yet");
+    std::vector<int> heap = m_numbers;
+    for (int i = heap.size() / 2 - 1; i >= 0; i--)
+    {
+        gen_heap(heap, i);
+    }
+    
+    //throw("TODO: not implemented yet");
+}
+
+void gen_heap(std::vector<int> &heap, int root)
+{
+    int left = 2 * root + 1;
+    int right = 2 * root + 2;
+    int largest = root;
+    int &val_current = heap.at(root);
+
+    if (left < heap.size())
+    {
+        if (heap.at(left) > heap.at(largest))
+            largest = left;
+    }
+    if (right < heap.size())
+    {
+        if (heap.at(right) > heap.at(largest))
+            largest = right;
+    }
+
+    if (largest != root)
+    {
+        std::swap(heap.at(largest), val_current);
+
+        gen_heap(heap, largest);
+    }
 }
